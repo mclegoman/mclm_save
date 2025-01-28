df296c937b075aa692468f953bcbb3065e3bbe42
dannytaylor <daniel@mclegoman.com>
2025-01-29 09:39:58 +1300

    tick and render events are now executed through the Execute class.

--- src/main/java/com/mclegoman/save/api/event/tick/TickEvents.java
+++ src/main/java/com/mclegoman/save/api/event/tick/TickEvents.java
@@ -7,6 +7,7 @@
 
 package com.mclegoman.save.api.event.tick;
 
+import com.mclegoman.save.api.event.Eventable;
 import org.quiltmc.loader.api.minecraft.ClientOnly;
 
 import java.util.HashMap;
@@ -14,15 +15,15 @@ import java.util.Map;
 
 @ClientOnly
 public final class TickEvents {
-	private static final Map<String, Tickable> startRegistry = new HashMap<>();
-	private static final Map<String, Tickable> endRegistry = new HashMap<>();
-	public static Map<String, Tickable> getStartRegistry() {
+	private static final Map<String, Eventable> startRegistry = new HashMap<>();
+	private static final Map<String, Eventable> endRegistry = new HashMap<>();
+	public static Map<String, Eventable> getStartRegistry() {
 		return startRegistry;
 	}
-	public static Map<String, Tickable> getEndRegistry() {
+	public static Map<String, Eventable> getEndRegistry() {
 		return endRegistry;
 	}
-	public static void register(Tick tick, String identifier, Tickable runnable) {
+	public static void register(Tick tick, String identifier, Eventable runnable) {
 		if (tick.equals(Tick.START)) startRegistry.put(identifier, runnable);
 		else if (tick.equals(Tick.END)) endRegistry.put(identifier, runnable);
 	}
---
+++ src/main/java/com/mclegoman/save/mixin/api/GameRendererMixin.java
@@ -0,0 +1,28 @@
+/*
+    Save
+    Contributor(s): dannytaylor
+    Github: https://github.com/MCLegoMan/mclm_save
+    Licence: GNU LGPLv3
+*/
+
+package com.mclegoman.save.mixin.api;
+
+import com.mclegoman.save.api.event.Execute;
+import net.minecraft.client.C_5664496;
+import net.minecraft.client.render.GameRenderer;
+import org.quiltmc.loader.api.minecraft.ClientOnly;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@ClientOnly
+@Mixin(GameRenderer.class)
+public abstract class GameRendererMixin {
+	@Shadow private C_5664496 minecraft;
+	@Inject(method = "m_8576613", at = @At(value = "INVOKE", target = "Ljava/lang/Thread;yield()V"))
+	private void save$render_end(float f, CallbackInfo ci) {
+		Execute.Render.end(this.minecraft);
+	}
+}
