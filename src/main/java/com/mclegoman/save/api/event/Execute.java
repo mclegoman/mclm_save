f8d1b6b12f6fb1efb4583507e47f1f88e9adfe8e
dannytaylor <daniel@mclegoman.com>
2025-01-29 22:34:06 +1300

    updated events, RenderEvents can now use Renderable or Eventable.

--- src/main/java/com/mclegoman/save/api/event/Execute.java
+++ src/main/java/com/mclegoman/save/api/event/Execute.java
@@ -15,45 +15,65 @@ import net.minecraft.client.C_5664496;
 
 public class Execute {
 	public static class Render {
-		public static void afterGameGui(C_5664496 minecraft) {
-			RenderEvents.getAfterGameGuiRegistry().forEach((identifier, eventable) -> {
+		public static void afterGameGui(C_5664496 minecraft, float f) {
+			RenderEvents.AfterGameGui.getRenderableRegistry().getRegistry().forEach((identifier, renderable) -> {
 				try {
+					minecraft.f_4267957.setupGuiState();
+					renderable.run(minecraft, f);
+				} catch (Exception error) {
+					Data.getVersion().sendToLog(LogType.ERROR, "An error occured whilst rendering " + identifier + " at after game gui render, removing from renderable registry:" + error.getLocalizedMessage());
+					RenderEvents.AfterGameGui.getRenderableRegistry().getRegistry().remove(identifier);
+				}
+			});
+			RenderEvents.AfterGameGui.getEventableRegistry().getRegistry().forEach((identifier, eventable) -> {
+				try {
+					minecraft.f_4267957.setupGuiState();
 					eventable.run(minecraft);
 				} catch (Exception error) {
-					Data.getVersion().sendToLog(LogType.ERROR, "An error occured whilst rendering " + identifier + " at after game gui render, removing from registry:" + error.getLocalizedMessage());
-					RenderEvents.getAfterGameGuiRegistry().remove(identifier);
+					Data.getVersion().sendToLog(LogType.ERROR, "An error occured whilst rendering " + identifier + " at after game gui render, removing from eventable registry:" + error.getLocalizedMessage());
+					RenderEvents.AfterGameGui.getEventableRegistry().getRegistry().remove(identifier);
 				}
 			});
 		}
-		public static void end(C_5664496 minecraft) {
-			RenderEvents.getEndRegistry().forEach((identifier, eventable) -> {
+		public static void end(C_5664496 minecraft, float f) {
+			RenderEvents.End.getRenderableRegistry().getRegistry().forEach((identifier, renderable) -> {
 				try {
+					minecraft.f_4267957.setupGuiState();
+					renderable.run(minecraft, f);
+				} catch (Exception error) {
+					Data.getVersion().sendToLog(LogType.ERROR, "An error occured whilst rendering " + identifier + " at end render, removing from renderable registry:" + error.getLocalizedMessage());
+					RenderEvents.AfterGameGui.getRenderableRegistry().getRegistry().remove(identifier);
+				}
+			});
+			RenderEvents.End.getEventableRegistry().getRegistry().forEach((identifier, eventable) -> {
+				try {
+					minecraft.f_4267957.setupGuiState();
 					eventable.run(minecraft);
 				} catch (Exception error) {
-					Data.getVersion().sendToLog(LogType.ERROR, "An error occured whilst rendering " + identifier + " at end render, removing from registry:" + error.getLocalizedMessage());
-					RenderEvents.getEndRegistry().remove(identifier);
+					Data.getVersion().sendToLog(LogType.ERROR, "An error occured whilst rendering " + identifier + " at end render, removing from eventable registry:" + error.getLocalizedMessage());
+					RenderEvents.AfterGameGui.getEventableRegistry().getRegistry().remove(identifier);
 				}
 			});
 		}
 	}
 	public static class Tick {
 		public static void start(C_5664496 minecraft) {
-			TickEvents.getStartRegistry().forEach((identifier, eventable) -> {
+			TickEvents.Start.getEventableRegistry().getRegistry().forEach((identifier, eventable) -> {
 				try {
 					eventable.run(minecraft);
 				} catch (Exception error) {
-					Data.getVersion().sendToLog(LogType.ERROR, "An error occured whilst ticking " + identifier + " at start tick, removing from registry:" + error.getLocalizedMessage());
-					TickEvents.getStartRegistry().remove(identifier);
+					Data.getVersion().sendToLog(LogType.ERROR, "An error occured whilst ticking " + identifier + " at start tick, removing from eventable registry:" + error.getLocalizedMessage());
+					RenderEvents.AfterGameGui.getEventableRegistry().getRegistry().remove(identifier);
 				}
 			});
 		}
 		public static void end(C_5664496 minecraft) {
-			TickEvents.getEndRegistry().forEach((identifier, eventable) -> {
+			TickEvents.End.getEventableRegistry().getRegistry().forEach((identifier, eventable) -> {
 				try {
 					eventable.run(minecraft);
 				} catch (Exception error) {
-					Data.getVersion().sendToLog(LogType.ERROR, "An error occured whilst ticking " + identifier + " at end tick, removing from registry:" + error.getLocalizedMessage());
-					TickEvents.getEndRegistry().remove(identifier);
+					Data.getVersion().sendToLog(LogType.ERROR, "An error occured whilst ticking " + identifier + " at end tick, removing from eventable registry:" + error.getLocalizedMessage());
+					RenderEvents.AfterGameGui.getEventableRegistry().getRegistry().remove(identifier);
 				}
 			});
 		}
